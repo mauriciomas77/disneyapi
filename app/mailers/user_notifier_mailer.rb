2e0a785982065f714942315b7c871e4c01019016
Mauricio Mas <mauriciomastonon@gmail.com>
2021-05-25 23:29:08 -0300

    Sendgrid changes 1

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,5 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
-  default :from => 'mydisneyapi@mauricio.com'
+  default :from => 'mauriciomastonon@gmail.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
